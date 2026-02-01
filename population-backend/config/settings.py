82d85a692e2e1c543bc30e0aa56c08dc175cc65e
kazamijimin <mendozanicknarry@gmail.com>
2026-02-01 10:46:02 +0800

    Vite and React setup

--- population-backend/config/settings.py
+++ population-backend/config/settings.py
@@ -76,13 +76,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_asTv94wQjVBg',
+        'HOST': 'ep-holy-water-a13umzty-pooler.ap-southeast-1.aws.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
+
 
 
 # Password validation
