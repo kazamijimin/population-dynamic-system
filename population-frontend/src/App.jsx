cc66e6a58a8d4a34cfeb0ad479f02dae472901c0
0ced0 <151804456+0ced0@users.noreply.github.com>
2026-02-10 08:16:38 +0800

    Initial dashboard page commit

--- population-frontend/src/App.jsx
+++ population-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/manager/Dashboard";
 
 function App() {
   return (
--- population-frontend/src/pages/manager/Dashboard.jsx
+++ population-frontend/src/pages/manager/Dashboard.jsx
@@ -1,23 +1,21 @@
-import { useEffect, useState } from "react";
-import api from "../api/axios";
+// import { useEffect, useState } from "react";
+// import api from "../../api/axios";
 
 export default function Dashboard() {
 
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
 
-  useEffect(() => {
-    api.get("/health/")
-      .then(res => setData(res.data))
-      .catch(err => console.error(err));
-  }, []);
+  // useEffect(() => {
+  //   api.get("/health/")
+  //     .then(res => setData(res.data))
+  //     .catch(err => console.error(err));
+  // }, []);
 
   return (
-    <div style={{ padding: 20 }}>
-      <h2>Population Dynamic System</h2>
-
-      {data && (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      )}
+    <div >
+      <div className="bg-red-100">
+        test
+      </div>
     </div>
   );
 }
